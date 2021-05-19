94da6ee173f4b8c0fb2ef0837c1ef7cab03fd481
emoreira96 <78985337+emoreira96@users.noreply.github.com>
2021-05-19 08:09:00 -0400

    Update big_box_grocers.py

--- big_box_grocers.py
+++ big_box_grocers.py
@@ -3,7 +3,7 @@ import itertools
  
 if __name__=='__main__':
     sc = SparkContext()
-    rdd = sc.textFile('/user/estrella.moreira/output_test0517/big_box_grocers/part-00000-af3cb71f-91aa-413f-a156-b57336618098-c000.csv')
+    rdd = sc.textFile('/user/estrella.moreira/output_test0517/big_box_grocers/*')
     header = rdd.first()
     rdd.sample(False, 0.01) \
         .coalesce(1) \
